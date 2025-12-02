b6bcbcdf1104193993897dc6c2f6fd7f5612e943
Danilo Mugosa <129607223+MugosaDanilo@users.noreply.github.com>
2025-12-02 16:24:00 +0100

    Prikaz ukupnog broja ECTS stranih predmeta na exportovanom word dokumentu

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'eabb2af3eee149631c9299187a9698946c9b97de',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -437,9 +437,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'eabb2af3eee149631c9299187a9698946c9b97de',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
